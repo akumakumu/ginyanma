8c6434feaad8865b3353e1834ecc604de399db08
Yy <akumakumu@gmail.com>
2024-02-13 23:24:48 +0700

    add express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require("graphql")
+var express = require("express")
+var { graphqlHTTP } = require("express-graphql")
+var { buildSchema } = require("graphql")
 
 var schema = buildSchema(`
     type Query {
@@ -6,16 +8,21 @@ var schema = buildSchema(`
     }
 `)
 
-var rootValue = {
+var root = {
     hello: ()=> {
         return "Hello World!"
     },
 }
 
-graphql({
-    schema,
-    source: "{ hello }",
-    rootValue,
-}).then(response => {
-    console.log(response)
-})
+var app = express()
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+)
+
+app.listen(4000)
+console.log("Running GraphQL at http://localhost:4000/graphql")
