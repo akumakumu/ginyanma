11a6bbe14903473ba723fe52a5fee729b3242cd9
Yy <akumakumu@gmail.com>
2024-05-12 21:36:40 +0700

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-const typeDefs = `
+const typeDefs = `#graphql
   type Profile {
     name: String
   }
