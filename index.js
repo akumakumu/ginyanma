38636cc98e163f4186b7258c68fbab3193581905
Yy <akumakumu@gmail.com>
2024-05-12 21:57:14 +0700

    Express + GraphQL

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+var { ruruHTML } = require("ruru/server")
+ 
+var schema = buildSchema(`
+  type Query {
+    name: String
+  }
+`)
+ 
+var root = {
+  name() {
+    return "Youne"
+  },
+}
+ 
+var app = express()
+ 
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+)
+
+app.get("/", (_req, res) => {
+  res.type("html")
+  res.end(ruruHTML({ endpoint: "/graphql" }))
+})
+
+app.listen(4000)
+console.log("Running a GraphQL API server at http://localhost:4000/graphql")
